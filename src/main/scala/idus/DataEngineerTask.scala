e3d9c18edaa8aa78d69cfbea534028c5dceba006
hjmoons <gywhd408@naver.com>
2021-11-11 16:19:41 +0900

    add question 3

--- src/main/scala/idus/DataEngineerTask.scala
+++ src/main/scala/idus/DataEngineerTask.scala
@@ -25,7 +25,7 @@ object DataEngineerTask {
     spark.udf.register("re_time", re_time)
 
     // 데이터 전처리 [event_time, event_type, user_id, user_session, day, hour, min, sec]
-    val df = load_data.drop("product_id").drop("category_id").drop("category_code").drop("brand").drop("price")
+    val df = load_data.drop("product_id", "category_id", "category_code", "brand", "price")
       .withColumn("event_time", expr("re_time(event_time)"))
       .withColumn("event_time", from_utc_timestamp($"event_time", "Asia/Seoul"))
       .withColumn("date", date_format($"event_time", "yyyy-MM-dd"))
@@ -41,15 +41,13 @@ object DataEngineerTask {
     val fst_df_count = fst_df.agg(countDistinct("user_id").as("num_user")).orderBy(desc("num_user"))
     fst_df_count.show(1, false)
 
-     */
-
     // Question 2.
     val sec_time = udf(UDFs.sec_time _)
     val sess_time = udf(UDFs.sess_time _)
     spark.udf.register("sec_time", sec_time)
     spark.udf.register("sess_time", sess_time)
 
-    val sec_df = df.drop("event_time").drop("event_type")
+    val sec_df = df.drop("event_time", "event_type")
       .filter($"date" === "2019-11-17")
       .withColumn("time", expr("sec_time(hour, min, sec)"))
 
@@ -73,15 +71,36 @@ object DataEngineerTask {
     */
 
     sec_df.show(false)
-    sec_df.createOrReplaceTempView("sessionTable")
+    sec_df.createOrReplaceTempView("session_table")
 
-    val sec_df_min_max = spark.sql("SELECT user_id, user_session as session_id, max(time) as maxTime, min(time) as minTime FROM sessionTable GROUP BY user_id, user_session")
+    val sec_df_min_max = spark.sql("SELECT user_id, user_session as session_id, max(time) as maxTime, min(time) as minTime FROM session_table GROUP BY user_id, user_session")
 
     sec_df_min_max.show()
     sec_df_min_max.createOrReplaceTempView("session_min_max")
 
     val sec_df_time = spark.sql("SELECT user_id, session_id, sess_time(maxTime, minTime) as session_time FROM session_min_max ORDER BY session_time DESC")
     sec_df_time.show(10, false)
+
+    */
+
+    // Question 3.
+    val dev_quarter = udf(UDFs.dev_quarter _)
+    val set_quarter_time = udf(UDFs.set_quarter_time _)
+    spark.udf.register("dev_quarter", dev_quarter)
+    spark.udf.register("set_quarter_time", set_quarter_time)
+
+    val trd_df = df.drop("event_type").drop("user_session")
+      .filter($"date" === "2019-11-17")
+      .withColumn("quarter", expr("dev_quarter(min)"))
+
+    trd_df.show(false)
+    trd_df.createOrReplaceTempView("quarter_table")
+
+    var trd_df_quarter = spark.sql("SELECT date, hour, quarter, count(DISTINCT(user_id)) as num_user FROM quarter_table GROUP BY date, hour, quarter ORDER BY hour, quarter")
+    trd_df_quarter = trd_df_quarter.withColumn("quarter_time", expr("set_quarter_time(date, hour, quarter)"))
+      .withColumn("num_user", $"num_user")
+      .drop("date", "hour", "quarter")
+    trd_df_quarter.show(100, false)
 
     spark.stop()
   }
--- src/main/scala/idus/UDFs.scala
+++ src/main/scala/idus/UDFs.scala
@@ -5,4 +5,21 @@ object UDFs {
   def re_time(time: String): String = time.replace(" UTC", "")
   def sec_time(hour: Int, min: Int, sec: Int): Int = hour * 3600 + min * 60 + sec
   def sess_time(maxTime: Int, minTime: Int): Int = maxTime - minTime
+  def dev_quarter(min: Int): Int = {
+    min/15 match {
+      case 0 => 0
+      case 1 => 1
+      case 2 => 2
+      case 3 => 3
+    }
+  }
+  def set_quarter_time(date: String, hour: Int, quarter: Int): String = {
+    if (hour < 10) {
+      if (quarter == 0)
+        date + " 0" + hour.toString + ":0" + (quarter * 15).toString + ":00"
+      else
+        date + " 0" + hour.toString + ":" + (quarter * 15).toString + ":00"
+    } else
+      date + " " + hour.toString + ":" + (quarter * 15).toString + ":00"
+  }
 }
